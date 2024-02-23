1e75e069dd0c3c2710289c0a496a838ddf4733da
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-23 15:37:11 +0100

    handling form submission

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,23 +1,49 @@
+import { useState } from 'react';
+
 import classes from './NewPost.module.css';
 
-function NewPost({ onBodyChange, onAuthorChange, onCancel }) {
+function NewPost({ onCancel }) {
 
-  return (
-    <form className={classes.form}>
-      <p>
-        <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onBodyChange} />
-      </p>
-      <p>
-        <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={onAuthorChange} />
-      </p>
-      <p className={classes.actions}>
-        <button type="button" onClick={onCancel}>Cancel</button>
-        <button>Submit</button>
-      </p>
-    </form>
-  );
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('')
+
+    function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+    }
+
+    function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+    }
+
+    function submitHandler (event) {
+        event.preventDefault();
+
+        const postData = {
+            body: enteredBody,
+            author: enteredAuthor
+        };
+
+        console.log(postData);
+
+        onCancel();
+    }
+
+    return (
+        <form className={classes.form} onSubmit={submitHandler}>
+        <p>
+            <label htmlFor="body">{}</label>
+            <textarea id="body" required rows={3} onChange={bodyChangeHandler} />
+        </p>
+        <p>
+            <label htmlFor="name">{}</label>
+            <input type="text" id="name" required onChange={authorChangeHandler} />
+        </p>
+        <p className={classes.actions}>
+            <button type="button" onClick={onCancel}>Cancel</button>
+            <button>Submit</button>
+        </p>
+        </form>
+    );
 }
 
 export default NewPost;
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-
 import Modal from './Modal';
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from './PostsList.module.css'
 
 function PostsList({ isPosting, onStopPosting }) {
-    const [enteredBody, setEnteredBody] = useState('');
-    const [enteredAuthor, setEnteredAuthor] = useState('')
-
-    function authorChangeHandler(event) {
-        setEnteredAuthor(event.target.value);
-    }
-
-    function bodyChangeHandler(event) {
-        setEnteredBody(event.target.value);
-    }
 
     return (
         <>
             {isPosting && (
                 <Modal onClose={onStopPosting}>
                     <NewPost 
-                        onBodyChange={bodyChangeHandler} 
-                        onAuthorChange={authorChangeHandler} 
                         onCancel={onStopPosting}
                     />
                 </Modal>
             )}
             <ul className={classes.posts}>
-                <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="Purity" body="NextJs is also a good library" />
             </ul>
         </>
