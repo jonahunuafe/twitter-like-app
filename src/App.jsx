d4905236a1d134037ba0f6b79b3aff326b542908
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-23 12:06:09 +0100

    passing data to components with props

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Post from "./components/Post";
 function App() {
   return (
     <main>
-      <Post />
-      <Post />
+      <Post author="Jonah" body="React.js is awesome!" />
+      <Post author="Purity" body="NextJs is also a good library" />
     </main>
   );
 }
