ac6e25923d3e7cce4d7b508cd869fa24d4a730e9
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-24 11:35:18 +0100

    dynamic routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Posts, {loader as postsLoader} from './routes/Posts'
 import RootLayout from './routes/RootLayout'
+import PostDetails, {loader as postDetailsLoader} from './routes/PostDetails'
 import NewPost, {action as newPostAction} from './routes/NewPost'
 import './index.css'
 
@@ -17,7 +18,7 @@ const router = createBrowserRouter([
       loader: postsLoader,
       children: [
         { path: '/create-post', element: <NewPost />, action: newPostAction },
-        
+        { path: '/:postId', element: <PostDetails/>, loader: postDetailsLoader  }
       ]
     },
   ]},
